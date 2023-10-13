a6f93eaeeec61e6c91f7cdd843307590cca6e501
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-10-13 20:45:44 +0300

    feature: added basic libraries and page layout (#2)
    * feature: initialized project
    
    * feature: initialized project
    
    * feature: added basic libraries
    
    * feature: added page layout for pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import PageLayout from '@/components/layout/page-layout/PageLayout';
+
 export default function Home() {
   return (
-    <main>
+    <PageLayout>
       <h1>Autolab</h1>
-    </main>
-  )
+    </PageLayout>
+  );
 }

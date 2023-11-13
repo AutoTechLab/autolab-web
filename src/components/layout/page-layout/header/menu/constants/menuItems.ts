28987a05a7b75b61e27b19e0fa39638cc77cc50f
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-11-13 15:03:20 +0200

    feature: created nextjs.yml (#5)
    * feature: initialize project (#1)
    
    * feature: initialized project
    
    * feature: initialized project
    
    * feature: added basic libraries and page layout (#2)
    
    * feature: initialized project
    
    * feature: initialized project
    
    * feature: added basic libraries
    
    * feature: added page layout for pages
    
    * Create nextjs.yml

---
+++ src/components/layout/page-layout/header/menu/constants/menuItems.ts
@@ -0,0 +1,18 @@
+export const menuItems = [
+  {
+    title: 'Головна',
+    url: '/',
+  },
+  {
+    title: 'Організація',
+    url: '/organisation',
+  },
+  {
+    title: 'Ціни',
+    url: '/prices',
+  },
+  {
+    title: 'Автомобілі',
+    url: '/vehicles',
+  },
+];

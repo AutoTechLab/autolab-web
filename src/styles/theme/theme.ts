a4f707582edec8e1d16aabd66a13f4a438a9c39c
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-11-16 22:57:11 +0200

    feature: added mui palette

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material';
+
+import palette from '@/styles/theme/constants/palette/palette';
+
+const theme = createTheme({
+  palette: palette,
+});
+
+export default theme;

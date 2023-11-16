36201336e83e60b2e25630322751869f6c26135e
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-11-16 23:25:11 +0200

    feature: added mui typography

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,9 +1,11 @@
 import { createTheme } from '@mui/material';
 
-import palette from '@/styles/theme/constants/palette/palette';
+import palette from '@/styles/theme/constants/palette';
+import typography from '@/styles/theme/constants/typography';
 
 const theme = createTheme({
-  palette: palette,
+  palette,
+  typography,
 });
 
 export default theme;

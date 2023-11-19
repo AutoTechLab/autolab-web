69f790769f8905cc1219599fa07486776ac53c29
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-11-19 13:06:16 +0200

    feature: added-skeleton-to-user-info (#23)

--- src/components/common/layout/page-layout/header/Header.tsx
+++ src/components/common/layout/page-layout/header/Header.tsx
@@ -1,23 +1,26 @@
 'use client';
 
-import { AppBar, Avatar, Box, Typography } from '@mui/material';
+import { AppBar, Typography } from '@mui/material';
 import Link from 'next/link';
 
 import Logo from '@/components/common/icons/Logo';
 import AuthButtons from '@/components/common/layout/page-layout/header/components/auth-buttons';
+import UserInfo from '@/components/common/layout/page-layout/header/components/user-info';
 import Menu from '@/components/common/layout/page-layout/header/menu/Menu';
 import useUser from '@/hooks/useUser';
 
 import * as styles from './Header.styles';
 
 const Header = () => {
-  const { user, isError, isLoading } = useUser();
+  const { user, isLoading, isError } = useUser();
 
   // const logout = async () => {
   //   storageUtil.deleteToken();
   //   await mutate();
   //   router.push('/');
   // };
+
+  console.log(isLoading, isError);
 
   return (
     <AppBar position="sticky" sx={styles.wrapper}>
@@ -26,27 +29,8 @@ const Header = () => {
         <Typography sx={styles.textLogo}>AUTOLAB</Typography>
       </Link>
       {user && !isError && <Menu />}
-      {!user || isError ? (
-        <AuthButtons />
-      ) : (
-        <>
-          {isLoading ? (
-            <Typography>Завантаження...</Typography>
-          ) : (
-            <Link href="/profile">
-              <Box sx={styles.userInfo}>
-                <Typography variant="h6" sx={styles.name}>
-                  {user.lastname} {user.firstname}
-                </Typography>
-                <Typography sx={styles.organisation}>Organistaion</Typography>
-              </Box>
-              <Avatar src="/images/avatar.jpg" sx={{ width: 40, height: 40 }}>
-                {user.lastname[0] + user.firstname[0]}
-              </Avatar>
-            </Link>
-          )}
-        </>
-      )}
+      {!user && isError && <AuthButtons />}
+      {(isLoading || user) && <UserInfo user={user} isLoading={isLoading} />}
     </AppBar>
   );
 };

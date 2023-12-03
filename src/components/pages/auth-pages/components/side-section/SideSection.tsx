8d962ba80a3c56047374712a55f4371732714874
Igor <114025058+igaryakqwe@users.noreply.github.com>
2023-12-03 12:59:00 +0200

    feature: added login-page

---
+++ src/components/pages/auth-pages/components/side-section/SideSection.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
+
+import Logo from '@/components/common/icons/Logo';
+import Button from '@/components/common/ui/button';
+import {
+  ButtonColor,
+  ButtonIcon,
+  ButtonSize,
+  ButtonVariant,
+} from '@/components/common/ui/button/types';
+import { LogoVariant } from '@/types/logo';
+
+import * as styles from './SideSection.styles';
+
+interface SideSectionProps {
+  text: 'login' | 'register';
+  link: string;
+}
+
+const SideSection: FC<SideSectionProps> = ({ text, link }) => {
+  const question = text === 'login' ? 'Не маєш акаунту?' : 'Вже маєш акаунт?';
+  const linkText = text === 'login' ? 'Зареєструватись' : 'Увійти';
+  return (
+    <Box sx={styles.section}>
+      <Box sx={styles.title}>
+        <Logo color="black" size={LogoVariant.MEDIUM} />
+        <Typography>AUTOLAB</Typography>
+      </Box>
+      <Typography sx={styles.question}>{question}</Typography>
+      <Link style={{ padding: 0, alignSelf: 'center' }} href={link}>
+        <Button
+          size={ButtonSize.MEDIUM}
+          variant={ButtonVariant.OUTLINED}
+          color={ButtonColor.SECONDARY}
+          icon={ButtonIcon.NONE}
+        >
+          {linkText}
+        </Button>
+      </Link>
+    </Box>
+  );
+};
+
+export default SideSection;

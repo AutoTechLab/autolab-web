f28f0b3720926b154db15af527c3e57edbe2c1a2
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-02-02 01:47:18 +0200

    bugfix: replaced the footer to the bottom of the page

---
+++ src/components/common/layout/page-layout/PageLayout.styles.ts
@@ -0,0 +1,11 @@
+import { SxProps, Theme } from '@mui/material/styles';
+
+export const layout: SxProps<Theme> = {
+  minHeight: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+};
+
+export const main: SxProps<Theme> = {
+  flex: '1 1 auto',
+};

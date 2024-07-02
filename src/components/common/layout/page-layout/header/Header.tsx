f918c24cd68880be616eb1644529c509fa3d55e1
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-07-03 02:10:07 +0300

    enhancement: made header responsible

--- src/components/common/layout/page-layout/header/Header.tsx
+++ src/components/common/layout/page-layout/header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { AppBar, Typography } from '@mui/material';
+import Bars3BottomRightIcon from '@heroicons/react/24/solid/Bars3BottomRightIcon';
+import { AppBar, IconButton, Typography, useMediaQuery } from '@mui/material';
 import Link from 'next/link';
 
 import Logo from '@/components/common/icons/Logo';
@@ -8,11 +9,13 @@ import AuthButtons from '@/components/common/layout/page-layout/header/component
 import UserInfo from '@/components/common/layout/page-layout/header/components/user-info';
 import Menu from '@/components/common/layout/page-layout/header/menu/Menu';
 import { useAuthContext } from '@/hooks/use-auth/auth-context/AuthContext';
+import theme from '@/styles/theme';
 
 import * as styles from './Header.styles';
 
 const Header = () => {
   const { user, isLoading, isError } = useAuthContext();
+  const isMobile = useMediaQuery(theme.breakpoints.down('desktopSemiMedium'));
 
   return (
     <AppBar position="sticky" sx={styles.wrapper}>
@@ -20,11 +23,19 @@ const Header = () => {
         <Logo />
         <Typography sx={styles.textLogo}>AUTOLAB</Typography>
       </Link>
-      {user && !isError && <Menu />}
-      {!user && !isLoading ? (
-        <AuthButtons />
+      {isMobile ? (
+        <IconButton>
+          <Bars3BottomRightIcon width={24} height={24} color="black" />
+        </IconButton>
       ) : (
-        <UserInfo user={user} isLoading={isLoading} />
+        <>
+          {user && !isError && <Menu />}
+          {!user && !isLoading ? (
+            <AuthButtons />
+          ) : (
+            <UserInfo user={user} isLoading={isLoading} />
+          )}
+        </>
       )}
     </AppBar>
   );

828fb71477b5da8c693a1adc4cfe488d6f48fa4f
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-07-07 19:17:31 +0300

    enhancement: improved responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,12 +6,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import Button from '@/components/common/ui/button';
-import {
-  ButtonColor,
-  ButtonIcon,
-  ButtonSize,
-  ButtonVariant,
-} from '@/components/common/ui/button/types';
 
 import * as styles from '../components/pages/NotFound.styles';
 
